a24e270f5ba94746a525e3d25f877448b89fa0cb
Jack Bendtsen <jackdbendtsen@gmail.com>
2025-04-29 00:11:03 +1000

    Started implementing compiler setup script

--- setup.py
+++ setup.py
@@ -7,9 +7,12 @@ import subprocess
 
 GIT = "git"
 HOST_GCC = "gcc"
+HOST_CLANG = "clang"
 
-def ensure_path_binary(program_name):
+def ensure_path_binary(program_name, alternative_name=None):
 	if shutil.which(program_name) is None:
+		if alternative_name is not None and shutil.which(alternative_name) is not None:
+			return
 		print("Could not find '" + program_name + "'.")
 		print("Make sure you have installed it from your package manager (else, search with google)")
 		print("and that '" + program_name + "' is on your PATH variable.")
@@ -17,10 +20,14 @@ def ensure_path_binary(program_name):
 
 def main(args):
 	ensure_path_binary(GIT)
-	ensure_path_binary(HOST_GCC)
+	ensure_path_binary(HOST_GCC, HOST_CLANG)
 
-	status = subprocess.run(["git", "clone", "-b", "releases/gcc-15", "--single-branch", "https://github.com/gcc-mirror/gcc.git"])
-	if status.returncode != 0:
-		return
+	if not os.path.isdir("gcc"):
+		status = subprocess.run(["git", "clone", "-b", "releases/gcc-15", "--single-branch", "https://github.com/gcc-mirror/gcc.git"])
+		if status.returncode != 0:
+			return
+
+	os.chdir("gcc")
+	subprocess.run(["./configure", "--target", "powerpc-750-eabi"])
 
 main(sys.argv)
