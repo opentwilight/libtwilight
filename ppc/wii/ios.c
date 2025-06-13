889898caee16176f2e5aa008e8aef94796c34528
Jack Bendtsen <jackdbendtsen@gmail.com>
2025-06-14 01:58:48 +1000

    Added IOS device list

--- ppc/wii/ios.c
+++ ppc/wii/ios.c
@@ -1,5 +1,53 @@
 #include <twilight.h>
 
+static const char *_default_ios_devices[] = {
+	"/dev/aes",
+	"/dev/boot2",
+	"/dev/di",
+	"/dev/es",
+	"/dev/flash",
+	"/dev/fs",
+	"/dev/hmac",
+	"/dev/listen",
+	"/dev/net",
+	"/dev/net/ip/bottom",
+	"/dev/net/ip/top",
+	"/dev/net/ip/top/Progress",
+	"/dev/net/kd",
+	"/dev/net/kd/request",
+	"/dev/net/kd/time",
+	"/dev/net/ncd/manage",
+	"/dev/net/ssl",
+	"/dev/net/ssl/code",
+	"/dev/net/usbeth",
+	"/dev/net/usbeth/top",
+	"/dev/net/wd",
+	"/dev/net/wd/command",
+	"/dev/net/wd/top",
+	"/dev/printserver",
+	"/dev/sdio",
+	"/dev/sdio/slot0",
+	"/dev/sdio/slot1",
+	"/dev/sha",
+	"/dev/stm",
+	"/dev/stm/eventhook",
+	"/dev/stm/immediate",
+	"/dev/usb",
+	"/dev/usb/ehc",
+	"/dev/usb/hid",
+	"/dev/usb/hub",
+	"/dev/usb/kbd",
+	"/dev/usb/msc",
+	"/dev/usb/oh0",
+	"/dev/usb/oh1",
+	"/dev/usb/shared",
+	"/dev/usb/usb",
+	"/dev/usb/ven",
+	"/dev/usb/wfssrv",
+	"/dev/wfsi",
+	"/dev/wl0"
+};
+
 TwStream TW_ListIosFolder(unsigned flags, const char *path, int pathLen) {
 	
 }
